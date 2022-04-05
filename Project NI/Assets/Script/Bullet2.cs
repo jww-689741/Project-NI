16728032102141c269f9ddcc62804b392798dd62
박건순 <98qkrrjstns@gmail.com>
2022-04-05 21:13:55 +0900

    Revert "탄환(곡선탄,산탄) 구현"
    This reverts commit a845cee88fd3847ed8d3acb6edd8a74b4b649d65.

--- Project NI/Assets/Script/Bullet2.cs
+++ Project NI/Assets/Script/Bullet2.cs
@@ -4,35 +4,34 @@ using UnityEngine;
 
 public class Bullet2 : MonoBehaviour
 {
-    public float firingAngle = 45.0f;  //포물선각도
-    public float gravity = 9.8f; //중력
-    public float weight = 1.3f;  //가중치
+    public float firingAngle = 45.0f;  //각도
+    public float gravity = 9.8f;  //중력
+    public float weight = 1.3f;   //가중치값
     // 탄환 발사 코루틴
     IEnumerator Shot(Vector3[] shotVecter)
     {
-        yield return new WaitForSeconds(0.5f);  // 0.5초 기다림
+        yield return new WaitForSeconds(0.5f);   // 0.5초 대기
+        // 대상거리
+        float target_Distance = 20f;
 
-        float target_Distance = 20f;  //포물선 거리
-
-        //지정된 각도에서 대상에 오브젝트를 던지는 데 필요한 속도를 계산합니다.
+        //지정된 각도에서 대상에 오브젝트를 던지는 데 필요한 속도를 계산
         float projectile_Velocity = target_Distance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / gravity);
 
-        //x축 y축 속도 계산
+        // 속도의 X Y 컴포넨트 추출
         float Vx = Mathf.Sqrt(projectile_Velocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad) * weight;
         float Vy = Mathf.Sqrt(projectile_Velocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad) * weight;
 
-        //비행시간 계산
+        // 비행 시간을 계산합니다.
         float filghtDuration = target_Distance / Vx + weight;
 
-        //비행시간동안 오브젝트 활성화
+        // 비행 시간동안 오브젝트 활성화
         float elapse_time = 0;
         while(elapse_time < filghtDuration)
         {
-            transform.Translate(0, (Vy - (gravity * elapse_time)) * Time.deltaTime, Vx * Time.deltaTime);
+            transform.Translate(0, (Vy - (gravity * elapse_time)) * Time.deltaTime, Vx * Time.deltaTime);  //포물선으로 탄환 발사
             elapse_time += Time.deltaTime;
             yield return null;
         }
-        
         gameObject.SetActive(false); // 비활성화
     }
 }
--- Project NI/Assets/Script/Bullet3.cs
+++ Project NI/Assets/Script/Bullet3.cs
@@ -15,18 +15,10 @@ public class Bullet3 : MonoBehaviour
         {
             timer += Time.deltaTime;
             if (timer > 2) break;
-            //transform.Translate(directionVector * Time.deltaTime * shotSpeed); // 탄환 발사
-            for(int i =0; i<5;i++)
-            {
-                this.transform.GetChild(i).gameObject.transform.Translate(directionVector * Time.deltaTime * shotSpeed);
-            }
+            transform.Translate(directionVector * Time.deltaTime * shotSpeed); // 탄환 발사
             yield return null; // 코루틴 딜레이 없음
         }
-        gameObject.SetActive(false);
-        //this.transform.GetChild(0).gameObject.SetActive(false); // 비활성화
-        //this.transform.GetChild(1).gameObject.SetActive(false);
-        //this.transform.GetChild(2).gameObject.SetActive(false);
-        //this.transform.GetChild(3).gameObject.SetActive(false);
-        //this.transform.GetChild(4).gameObject.SetActive(false);
+
+        gameObject.SetActive(false); // 비활성화
     }
 }
