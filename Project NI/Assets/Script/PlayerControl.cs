1bd06afacc9426927341eb8bdb64a3fe3d73e5b6
DESKTOP-EGHOF5E\bh597 <bh5975@naver.com>
2022-04-10 21:26:41 +0900

    적 이동 패턴 추가 2차 및 곡선 이동, 적 공격 V.1

---
+++ Project NI/Assets/Script/PlayerControl.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float repeaterInterval; // 연사속도
+    public float movementSpeed; // 이동속도
+
+    private bool repeaterLock; // 연사 여부
+    private delegate void Control();
+    Control control;
+    private Rigidbody playerRigidbody;
+    private Camera camera;
+    private RaycastHit hit;
+    private Vector3 shotPoint;
+
+    private void Start()
+    {
+        repeaterLock = true; // 연사 활성화
+        control = Aiming;
+        control += Movement;
+        control += Shot;
+        playerRigidbody = this.GetComponent<Rigidbody>();
+        camera = Camera.main; // 메인 카메라 컴포넌트를 가져옴
+    }
+
+    private void Update()
+    {
+        control();
+    }
+
+    // 조준
+    private void Aiming() {
+
+        // 화면 상의 마우스 포인터 위치로 레이캐스팅
+        if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            shotPoint = new Vector3(hit.point.x * 2f, hit.point.y, 15);
+        }
+    }
+
+
+    // 탄환 발사
+    private void Shot()
+    {
+        if (Input.GetMouseButtonDown(0)) // 마우스 좌클릭 유지중일 때
+        {
+            StartCoroutine("Repeater");
+        }
+        else if (Input.GetMouseButtonUp(0)) repeaterLock = false; // 마우스 좌클릭에서 손을 뗄때
+    }
+
+    // 플레이어 이동
+    private void Movement()
+    {
+        // 플레이어가 이동하는 방향과 속도를 삽입한 벡터 - 마우스 휠 컨트롤 삽입 필요
+        float movementX = Input.GetAxis("XMove");
+        float movementY = Input.GetAxis("YMove");
+        float movementZ = Input.GetAxis("ZMove");
+        Vector3 movementVector = new Vector3(movementX, movementY, movementZ) * movementSpeed;
+        playerRigidbody.velocity = movementVector;
+    } 
+
+    // 탄환 연사 코루틴
+    IEnumerator Repeater()
+    {
+        repeaterLock = true; // 연사 활성화
+        while (repeaterLock)
+        {
+            SetBullet(TestObjectManager.instance.GetBullet("Test")); // 탄환 발사
+            yield return new WaitForSeconds(repeaterInterval); // 연사시간만큼 대기
+        }
+    }
+
+    // 탄환 오브젝트 위치 지정, 회전각도 설정, 오브젝트 활성화, 실제 발사 로직 작동
+    private void SetBullet(GameObject bullet)
+    {
+        if (bullet == null) return; // 받아올 탄환이 없을 경우 반환
+
+        bullet.transform.position = this.transform.position; // 위치 지정
+        bullet.transform.rotation = this.transform.rotation; // 회전각 지정
+        bullet.SetActive(true); // 활성화
+        bullet.GetComponent<Bullet>().StartCoroutine("Shot", shotPoint); // 탄환 동작 로직 코루틴 시작
+    }
+
+}
---
+++ Project NI/Assets/Script/TestObjectManager.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TestObjectManager : MonoBehaviour
+{
+    // 싱글톤
+    public static TestObjectManager instance;
+
+    // 생성 시 부모 오브젝트 설정
+    [Header("Parent Object")]
+    [SerializeField]
+    private Transform BulletPool;
+
+    [Header("Parent Object")]
+    [SerializeField]
+    private Transform EnemyPool;
+
+    // 싱글톤 적용
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // 게임 종료 시 메모리 할당해제
+    private void OnDestroy()
+    {
+        MemoryClear();
+    }
+
+    public BulletInfo[] bulletinfo = null; // 탄환 오브젝트 정보의 배열
+    public List<GameObject> bulletList; // 생성한 탄환 리스트
+
+    public EnemyInfo[] enemyinfo = null; // 적 오브젝트 정보의 배열
+    public List<GameObject> enemyList; // 생성한 적 리스트
+
+    // 오브젝트 사전 생성
+    private void Start()
+    {
+        // 탄환 세팅
+        if (bulletinfo.Length < 1) SetBullet(bulletinfo[0].prefab, bulletinfo[0].count, bulletinfo[0].name); // 리스트의 내용이 하나일 경우
+        else // 리스트의 내용이 둘 이상인 경우
+        {
+            for (int i = 0; i < bulletinfo.Length; i++)
+            {
+                SetBullet(bulletinfo[i].prefab, bulletinfo[i].count, bulletinfo[i].name);
+            }
+        }
+
+        // 적 세팅
+        if (enemyinfo.Length < 1) SetEnemy(enemyinfo[0].prefab, enemyinfo[0].count, enemyinfo[0].name); // 리스트의 내용이 하나일 경우
+        else // 리스트의 내용이 둘 이상인 경우
+        {
+            for (int i = 0; i < enemyinfo.Length; i++)
+            {
+                SetEnemy(enemyinfo[i].prefab, enemyinfo[i].count, enemyinfo[i].name);
+            }
+        }
+    }
+
+    // 탄환 생성, 속성 입력, 리스트 삽입
+    public void SetBullet(GameObject bullet, int count, string name)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject clone = Instantiate(bullet) as GameObject; // 클론 생성
+            clone.transform.name = name; // 이름
+            clone.transform.localPosition = Vector3.zero; // 위치
+            clone.SetActive(false); // 비활성화
+            clone.transform.parent = BulletPool; // 부모 오브젝트 설정
+            bulletList.Add(clone); // 리스트 삽입
+        }
+    }
+
+    // 적 생성, 속성 입력, 리스트 삽입
+    public void SetEnemy(GameObject enemy, int count, string name)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject clone = Instantiate(enemy) as GameObject; // 클론 생성
+            clone.transform.name = name; // 이름
+            clone.transform.localPosition = Vector3.zero; // 위치
+            clone.SetActive(false); // 비활성화
+            clone.transform.parent = EnemyPool; // 부모 오브젝트 설정
+            enemyList.Add(clone); // 리스트 삽입
+        }
+    }
+
+    // 탄환 호출
+    public GameObject GetBullet(string name)
+    {
+        if (bulletList == null) return null; // 리스트가 비어있으면 null 반환
+
+        int listCount = bulletList.Count;
+        for (int i = 0; i < listCount; i++)
+        {
+            if (name != bulletList[i].name) continue; // 찾는 이름의 탄환이 없으면 넘어감
+
+            GameObject targetBullet = bulletList[i]; // 찾는 탄환의 오브젝트
+
+            if (targetBullet.activeSelf == true) // 탄환이 활성화 시
+            {
+                if (i == listCount - 1) // 리스트 내 모든 탄환이 활성화 상태일 때
+                {
+                    SetBullet(targetBullet, 1, targetBullet.name); // 새로운 탄환 1개 추가
+                    return bulletList[i + 1]; // 새로 생성된 탄환 반환
+                }
+                continue;
+            }
+            return bulletList[i]; // 비활성화 상태인 탄환 반환
+        }
+        return null; // 호출 할 탄환의 이름을 찾지 못한 경우 null반환
+    }
+
+    // 적 다중 호출
+    public GameObject[] GetEnemy(string name, int count)
+    {
+        GameObject[] target = new GameObject[count]; // 가져갈 적 오브젝트 저장용 배열
+        GameObject targetEnemy; // 임시 저장용 오브젝트
+
+        for (int z = 0; z < count; z++) // 필요로 하는 오브젝트 수 만큼 반복
+        {
+            for (int i = 0 + z; i < enemyList.Count; i++) // 현재 오브젝트 풀링에 저장되어 있는 적 오브젝트 수 만큼 반복
+            {
+                if(enemyList[i].activeSelf == false) // 오브젝트 풀링의 i번째 적 오브젝트가 비활성화 상태일 때
+                {
+                    if (enemyList[i].name == name) // 해당 오브젝트와 원하는 오브젝트의 이름이 같을 때
+                    {
+                        targetEnemy = enemyList[i]; // target에 해당 오브젝트 임시 저장
+                        int j = 0; // 선언을 for문이 아닌 여기서 하는 이유는 아래의 if문에서 사용하기 위함
+
+                        for (j = 0; j < z; j++) // 가져가기로한 적 오브젝트 수만큼 반복
+                        {
+                            if (i == enemyList.Count - 1) // 이번이 리스트 마지막이라면, 해당 부분으로 인해 1개의 추가 요소 생성됨, 수정필요
+                            {
+                                SetEnemy(targetEnemy, 1, targetEnemy.name); // 새로 만들기
+                            }
+
+                            if (targetEnemy == target[j]) // 해당 오브젝트가 이미 가져갈 목록에 있다면 
+                            {
+                                break; // 반복문 종료
+                            }
+                        }
+
+                        if (j == z) // 반복문이 끝까지 돌았을 때, 즉 해당 오브젝트가 가져갈 목록에 없다면
+                        {
+                            target[z] = targetEnemy; // 해당 오브젝트를 가져갈 목록에 넣는다.
+                            break; // 반복문 종료
+                        }
+                    }
+                }
+            }
+        }
+
+        return target;
+    }
+
+    // 메모리 할당 해제
+    public void MemoryClear()
+    {
+        if (bulletList == null) return; // 리스트가 비어있으면 반환
+
+        int listCount = bulletList.Count;
+
+        for (int i = 0; i < listCount; i++)
+        {
+            GameObject.Destroy(bulletList[i]); // 리스트 내에 오브젝트 삭제
+        }
+        bulletList = null; // 리스트 비우기
+
+        if (enemyList == null) return; // 리스트가 비어있으면 반환
+
+        listCount = enemyList.Count;
+
+        for (int i = 0; i < listCount; i++)
+        {
+            GameObject.Destroy(enemyList[i]); // 리스트 내에 오브젝트 삭제
+        }
+        enemyList = null; // 리스트 비우기
+    }
+}
